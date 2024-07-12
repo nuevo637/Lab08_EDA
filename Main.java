fb1e1252064e4d1477da0fa531cb7a4d2d692b3a
nuevo_63 <ajrahuacso@gmail.com>
2024-07-11 19:49:43 -0500

    Main actualizado

--- Main.java
+++ Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a phrase to codify:");
-        Map<Character, Integer> frequencies = calcularFrecuencias(sc.nextLine());
+        System.out.println("Ingrese la palabra a Codificar:");
+        String frase = sc.nextLine();
+
+        // Calcular frecuencias de caracteres en la frase
+        Map<Character, Integer> frequencies = calcularFrecuencias(frase);
 
         // Crear lista enlazada de nodos de Huffman
         LinkedListHuffman list = new LinkedListHuffman();
@@ -13,7 +16,7 @@ public class Main {
             list.add(new HuffmanNode(entry.getKey(), entry.getValue()));
         }
 
-        // Ordenar la lista
+        // Ordenar la lista según las frecuencias
         list.sort();
 
         // Construir el árbol de Huffman
@@ -23,21 +26,33 @@ public class Main {
         Map<Character, String> huffmanCodes = new HashMap<>();
         list.assignHuffmanCodes(root, "", huffmanCodes);
 
-        // Imprimir los códigos de Huffman
+        // Codificar la frase usando los códigos Huffman
+        StringBuilder fraseCodificada = new StringBuilder();
+        for (char c : frase.toCharArray()) {
+            fraseCodificada.append(huffmanCodes.get(c));
+        }
+
+        // Imprimir los códigos de Huffman generados
+        System.out.println("Códigos Huffman generados:");
         for (Map.Entry<Character, String> entry : huffmanCodes.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+
+        // Imprimir la frase codificada
+        System.out.println("Frase codificada:");
+        System.out.println(fraseCodificada.toString());
     }
 
     private static Map<Character, Integer> calcularFrecuencias(String frase) {
         Map<Character, Integer> frecuencias = new HashMap<>();
 
         for (char c : frase.toCharArray()) {
-            c = Character.toLowerCase(c);
-            frecuencias.put(c, frecuencias.getOrDefault(c, 0) + 1);
+            if (Character.isLetterOrDigit(c)) {
+                c = Character.toLowerCase(c);
+                frecuencias.put(c, frecuencias.getOrDefault(c, 0) + 1);
+            }
         }
 
         return frecuencias;
     }
-
 }
