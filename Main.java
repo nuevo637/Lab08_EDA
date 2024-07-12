345166a5ba83eb8b9ae9ac4fcc3e8365d02796c7
gusCreator <lgsc21211@gmail.com>
2024-07-11 19:03:10 -0500

    Se prueba la codificación de los caracteres

--- Main.java
+++ Main.java
@@ -1,11 +1,36 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Map<Character, Integer> frequencies = new HashMap<>();
+        frequencies.put(' ', 5);
+        frequencies.put('a', 9);
+        frequencies.put('c', 2);
+        frequencies.put('e', 1);
+        frequencies.put('j', 1);
+        frequencies.put('l', 2);
+        frequencies.put('s', 1);
+        frequencies.put('t', 2);
 
-        System.out.println("Ingrese la frase a codificar: ");
-        String frase = sc.nextLine();
-        
-        sc.close();
+        // Crear lista enlazada de nodos de Huffman
+        LinkedListHuffman list = new LinkedListHuffman();
+        for (Map.Entry<Character, Integer> entry : frequencies.entrySet()) {
+            list.add(new HuffmanNode(entry.getKey(), entry.getValue()));
+        }
+
+        // Ordenar la lista
+        list.sort();
+
+        // Construir el árbol de Huffman
+        HuffmanNode root = list.buildHuffmanTree();
+
+        // Asignar códigos de Huffman
+        Map<Character, String> huffmanCodes = new HashMap<>();
+        list.assignHuffmanCodes(root, "", huffmanCodes);
+
+        // Imprimir los códigos de Huffman
+        for (Map.Entry<Character, String> entry : huffmanCodes.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
+
 }
